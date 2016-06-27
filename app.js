bacd66c3e68c3436d129c5257fce3ae8ef46be80
adina diaconescu <adinachirita@yahoo.com>
2016-06-27 08:41:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = 3003;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/',function(req,res){
+    res.send('hello adina')
+});
+
+app.get('/books',function(req,res){
+    res.send('hello books')
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
