247b443293c9fb71cff8243399d8dcc701eda1c4
Adina Chirita <adch@enea.se>
2016-07-05 09:38:05 +0300

    commit

--- app.js
+++ app.js
@@ -3,51 +3,24 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3003;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Author',
+    Text: 'Author'
+}];
 
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
-
-var bookRouter = express.Router();
 
 app.set('views', './src/views');
 
 
 app.set('view engine', '.ejs');
 
-var books = [{
-    title: 'Cartea cartilor',
-    author: 'the people'
-}, {
-    title: 'Cartea 7',
-    author: 'R2D2'
-},
-    {
-        title: 'Cartea xx',
-        author: 'the other people'
-    }];
-
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'Books !',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            books : books
-        });
-
-    });
-
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello singleBooks');
-    });
-
-app.use('/Books', bookRouter);
+app.use('/books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -63,7 +36,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/books', function (req, res) {
+app.get('/Books', function (req, res) {
     res.send('hello books');
 });
 
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,44 @@
+/**
+ * Created by adch on 05.07.2016.
+ */
+var express = require('express');
+
+var bookRouter = express.Router();
+var router = function (nav) {
+    var books = [{
+        title: 'Cartea cartilor',
+        author: 'the people'
+    }, {
+        title: 'Cartea 7',
+        author: 'R2D2'
+    },
+        {
+            title: 'Cartea xx',
+            author: 'the other people'
+        }];
+
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('bookListView', {
+                title: 'Books !',
+                nav: nav,
+                books : books
+            });
+
+        });
+
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render('bookView', {
+                title: 'Books !',
+                nav: nav,
+                book : books[id]
+            });
+        });
+    return bookRouter;
+};
+
+
+
+module.exports = router;
